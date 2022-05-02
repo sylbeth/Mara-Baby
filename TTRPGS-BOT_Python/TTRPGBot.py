30d15c9b09fc60549dea50808189fde362f13d6d
Sílvia Elisabeth Romero García <sylvielizshadow@gmail.com>
2022-05-02 15:36:02 +0200

    add brief and description to commands, rework class TTRPGBot

--- TTRPGS-BOT_Python/TTRPGBot.py
+++ TTRPGS-BOT_Python/TTRPGBot.py
@@ -1,12 +1,22 @@
 import discord
 from discord.ext import commands
 
-command_prefix = 'ttr'
-description = '''A multi-purpose bot for all kinds of tabletop role-playing games.
-It's a nice child, take care of my child.'''
-intents = discord.Intents.default()
-intents.members = True
 
 class TTRPGBot(commands.Bot):
-	def __init__(self, command_prefix = command_prefix, description = description, intents = intents):
-		super().__init__(command_prefix, description, intents)
+	def __init__(self, command_prefix=commands.when_mentioned_or('ttr'), description=
+	'''A multi-purpose bot for all kinds of tabletop role-playing games.
+	It's a nice child, take care of my child.'''):
+		super().__init__(command_prefix=command_prefix, description=description)
+		self.strip_after_prefix = True
+	
+	async def on_ready(self):
+		print('{0.user.name}\'s body is ready'.format(self))
+	
+	@commands.command(name='hello', brief='Says hello back.', description='Says hello back like a good child would do.')
+	async def hello(context:commands.Context):
+		await context.send('Hello World.')
+
+	@commands.command(name='replace', brief='Replaces your message.', description='Replaces your message because this was me testing how to replace messages.')
+	async def replace(context:commands.Context):
+		await context.message.delete()
+		await context.send('I am replacing a message by {0.author}'.format(context))
--- TTRPGS-BOT_Python/bot.py
+++ TTRPGS-BOT_Python/bot.py
@@ -2,25 +2,25 @@ import discord
 from discord.ext import commands
 import os
 
-command_prefix = 'ttr '
+command_prefix = 'ttr'
 description = '''A multi-purpose bot for all kinds of tabletop role-playing games.
 It's a nice child, take care of my child.'''
-intents = discord.Intents.default()
-intents.members = True
 
-bot = commands.Bot(command_prefix=command_prefix, description=description, intents=intents)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(command_prefix), description=description)
+
+bot.strip_after_prefix = True
 
 @bot.event
 async def on_ready():
 	print('{0.user.name}\'s body is ready'.format(bot))
 
-@bot.command()
-async def hello(context):
-	await context.send('Hello World')
+@bot.command(name='hello', brief='Says hello back.', description='Says hello back like a good child would do.')
+async def hello(context:commands.Context):
+	await context.send('Hello World.')
 
-@bot.command()
-async def replace(context):
+@bot.command(name='replace', brief='Replaces your message.', description='Replaces your message because this was me testing how to replace messages.')
+async def replace(context:commands.Context):
 	await context.message.delete()
-	await context.send('I am replacing your message')
+	await context.send('I am replacing a message by {0.author}'.format(context))
 
 bot.run(os.getenv('TOKEN'))
