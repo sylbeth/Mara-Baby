9232b5f25b598db34eb77969308744dec2d9817e
Sílvia Elisabeth Romero García <sylvielizshadow@gmail.com>
2022-05-02 18:28:41 +0200

    add 1046 command

--- TTRPGS-BOT_Python/TTRPGBot.py
+++ TTRPGS-BOT_Python/TTRPGBot.py
@@ -1,22 +1,31 @@
-import discord
 from discord.ext import commands
 
-
-class TTRPGBot(commands.Bot):
-	def __init__(self, command_prefix=commands.when_mentioned_or('ttr'), description=
-	'''A multi-purpose bot for all kinds of tabletop role-playing games.
-	It's a nice child, take care of my child.'''):
+class BotExtension(commands.Bot):
+	def __init__(self, command_prefix, description):
 		super().__init__(command_prefix=command_prefix, description=description)
 		self.strip_after_prefix = True
 	
 	async def on_ready(self):
 		print('{0.user.name}\'s body is ready'.format(self))
-	
-	@commands.command(name='hello', brief='Says hello back.', description='Says hello back like a good child would do.')
+
+class TTRPGBot():
+	command_prefix = 'ttr'
+	description = '''A multi-purpose bot for all kinds of tabletop role-playing games.
+	It's a nice child, take care of my child.'''
+	bot = BotExtension(command_prefix=command_prefix, description=description)
+
+	@bot.command(name='hello', brief='Says hello back.', description='Says hello back like a good child would do.')
 	async def hello(context:commands.Context):
 		await context.send('Hello World.')
 
-	@commands.command(name='replace', brief='Replaces your message.', description='Replaces your message because this was me testing how to replace messages.')
+	@bot.command(name='replace', brief='Replaces your message.', description='Replaces your message because this was me testing how to replace messages.')
 	async def replace(context:commands.Context):
 		await context.message.delete()
-		await context.send('I am replacing a message by {0.author}'.format(context))
+		await context.send('I am replacing a message by {0.author.name}'.format(context))
+	
+	@bot.command(name='1046', brief='Says hi to their moms.', description='Says hi to either LuLu or Sylbeth.')
+	async def replace(context:commands.Context):
+		if context.author.id == 245623966088167426 or context.author.id == 680142975078105136:
+			await context.send('Hi mom! Hope you are having a nice day, I wuv you!')
+		else:
+			await context.send('You aren\'t my mom, you shouldn\'t be using this.')
--- TTRPGS-BOT_Python/bot.py
+++ TTRPGS-BOT_Python/bot.py
@@ -1,26 +1,6 @@
-import discord
-from discord.ext import commands
+from TTRPGBot import TTRPGBot
 import os
 
-command_prefix = 'ttr'
-description = '''A multi-purpose bot for all kinds of tabletop role-playing games.
-It's a nice child, take care of my child.'''
-
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(command_prefix), description=description)
-
-bot.strip_after_prefix = True
-
-@bot.event
-async def on_ready():
-	print('{0.user.name}\'s body is ready'.format(bot))
-
-@bot.command(name='hello', brief='Says hello back.', description='Says hello back like a good child would do.')
-async def hello(context:commands.Context):
-	await context.send('Hello World.')
-
-@bot.command(name='replace', brief='Replaces your message.', description='Replaces your message because this was me testing how to replace messages.')
-async def replace(context:commands.Context):
-	await context.message.delete()
-	await context.send('I am replacing a message by {0.author}'.format(context))
+bot = TTRPGBot().bot
 
 bot.run(os.getenv('TOKEN'))
